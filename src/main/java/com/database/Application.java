6c2ff0139d24477a24ccf07ff72f32c715e3739e
snoopy100 <nkotch@gmail.com>
2025-03-05 21:43:26 -0500

    added more elements to field.fxml

--- src/main/java/com/database/Application.java
+++ src/main/java/com/database/Application.java
@@ -22,9 +22,9 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         //URL url = getClass().getResource("/com/database/view.fxml");
-	URL url = getClass().getResource("/com/database/Field.fxml");
+	    //URL url = getClass().getResource("Field.fxml");
         
-	FXMLLoader fxmlLoader = new FXMLLoader(url);
+	    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Field.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Title Goes Here");
         stage.setScene(scene);
--- src/main/java/com/database/FieldController.java
+++ src/main/java/com/database/FieldController.java
@@ -1,9 +1,13 @@
 package com.database;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 
 public class FieldController {
+    @FXML public TextField MRNField;
+    @FXML public TextField dateField;
+    @FXML public TextField nameField;
     @FXML AnchorPane pane;
     //@FXML TextField nameField;
     //@FXML TextField MRNField;
